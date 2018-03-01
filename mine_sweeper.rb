c2c3c1e13328b6d671f9d9c79d963649279cc572
Gaston Ramos <ramos.gaston@gmail.com>
2018-03-01 10:25:40 -0300

    Mine the board.
    Use 2 mines per row by default.

--- mine_sweeper.rb
+++ mine_sweeper.rb
@@ -5,7 +5,7 @@ class MineSweeper
   def initialize
     @board = []
 
-    5.times do |t|
+    6.times do |t|
       @board[t] = [0, 0, 0, 0, 0, 0]
     end
   end
@@ -19,5 +19,19 @@ class MineSweeper
       { f: ( row +1 ), c: (col -1) }, { f: ( row +1 ), c: col },
       { f: ( row +1 ), c: (col +1) },
     ]
+  end
+
+  def mine!
+    @board.size.times do |r|
+      c1 = rand(6)
+      c2 = rand(6)
+
+      while(c2 == c1) do
+        c2 = rand(6)
+      end
+
+      @board[r][c1] = 'x'
+      @board[r][c2] = 'x'
+    end
   end
 end
--- test/minesweeper_test.rb
+++ test/minesweeper_test.rb
@@ -1,9 +1,9 @@
 require_relative '../mine_sweeper'
 
-test 'Get an empty board' do
+test 'Get an empty board 6x6 by default' do
   empty_board = []
 
-  5.times do |t|
+  6.times do |t|
     empty_board[t] = [0, 0, 0, 0, 0, 0]
   end
 
@@ -16,4 +16,20 @@ test 'Get the neighbors of a cell' do
   assert ms.neighbors(3, 2) == [ {f: 2, c: 1}, {f: 2, c: 2}, {f: 2, c: 3},
                                  {f: 3, c: 1}, {f: 3, c: 3},
                                  {f: 4, c: 1}, {f: 4, c: 2}, {f: 4, c: 3} ]
+end
+
+test 'Mine the board with 2 mines per row by default' do
+  ms = MineSweeper.new
+
+  ms.mine!
+
+  6.times do |r|
+    mine_counter = 0
+
+    6.times do |c|
+      mine_counter += 1 if ms.board[r][c] == 'x'
+    end
+
+    assert mine_counter == 2
+  end
 end
