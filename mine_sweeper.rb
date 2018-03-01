c00a599ded08a63f0e7de3add71d40abb4048a8b
Gaston Ramos <ramos.gaston@gmail.com>
2018-03-01 09:56:55 -0300

    Add neighbors method and test.

--- mine_sweeper.rb
+++ mine_sweeper.rb
@@ -9,4 +9,15 @@ class MineSweeper
       @board[t] = [0, 0, 0, 0, 0, 0]
     end
   end
+
+  def neighbors(row, col)
+    [ { f: ( row -1 ), c: (col -1) }, { f: ( row -1 ), c: col },
+      { f: ( row -1 ), c: (col +1) },
+
+      { f: ( row ), c: (col -1) }, { f: ( row ), c: col +1},
+
+      { f: ( row +1 ), c: (col -1) }, { f: ( row +1 ), c: col },
+      { f: ( row +1 ), c: (col +1) },
+    ]
+  end
 end
--- test/minesweeper_test.rb
+++ test/minesweeper_test.rb
@@ -9,3 +9,11 @@ test 'Get an empty board' do
 
   assert MineSweeper.new.board == empty_board
 end
+
+test 'Get the neighbors of a cell' do
+  ms = MineSweeper.new
+
+  assert ms.neighbors(3, 2) == [ {f: 2, c: 1}, {f: 2, c: 2}, {f: 2, c: 3},
+                                 {f: 3, c: 1}, {f: 3, c: 3},
+                                 {f: 4, c: 1}, {f: 4, c: 2}, {f: 4, c: 3} ]
+end
