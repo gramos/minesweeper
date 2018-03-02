c5834ed4668c4756d39b38bb8f7efcba6c27b069
Gaston Ramos <ramos.gaston@gmail.com>
2018-03-02 15:50:23 -0300

    Detect when the game is over.

--- public/js/base.js
+++ public/js/base.js
@@ -24,10 +24,13 @@ function fill_board(board_json) {
             r = this.id.split("c")[0].split("r")[1];
 
               if (board_json[r][c] == "x") {
+                this.classList.add("sqExposed");
                 this.innerHTML = '<i class="fas fa-bomb sqExploded"></i>';
+                open_modal();
               } else if (board_json[r][c] == "0"){
                   get_neighbors(r, c, board_json);
               } else {
+                  this.classList.add("sqExposed");
                   this.innerHTML = board_json[r][c];
               }
           });
@@ -35,10 +38,21 @@ function fill_board(board_json) {
     }
 }
 
+function open_modal(){
+  var modal = document.getElementById('modalMessage');
+  var span = document.getElementsByClassName("close")[0];
+
+   modal.style.display = "block";
+   span.onclick = function() {
+   modal.style.display = "none";
+   document.getElementById("boardTable").remove();
+   get_board();
+  }
+}
 function get_neighbors(row, col, board_json) {
     var xhr = new XMLHttpRequest();
 
-    var url = "http://microteatro.lacasa.tech/change_mine_sweeper/" + row +
+    var url = "http://minesweeper.lacasa.tech/mine_sweeper/" + row +
               "/" + col + "/" + JSON.stringify(board_json);
 
     xhr.onreadystatechange = function () {
@@ -48,7 +62,7 @@ function get_neighbors(row, col, board_json) {
               var element = adjacents[i];
               cell = document.getElementById("r"+ element['r']  + "c" + element['c']);
               cell.innerHTML = element['data'];
-              cell.className = "sq sqExposed";
+              cell.classList.add("sqExposed");
           }
 
       }
@@ -61,12 +75,11 @@ function get_neighbors(row, col, board_json) {
 
 function get_board() {
     var xhr = new XMLHttpRequest();
-    var url = "http://microteatro.lacasa.tech/mine_sweeper";
+    var url = "http://minesweeper.lacasa.tech/mine_sweeper";
 
     var board_json = '';
 
     xhr.onreadystatechange = function () {
-
         if (xhr.readyState === 4 && xhr.status === 200) {
             board_json = JSON.parse(xhr.responseText);
             fill_board(board_json);
